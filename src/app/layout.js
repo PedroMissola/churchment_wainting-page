3f66a564b4f46741846e89efacb718f578c54fa2
PedroMissola <missolapedro@gmail.com>
2025-05-22 14:44:26 -0300

    Adicição das fontes e viewport no layout e melhora das metadatas

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,16 @@
 import "./globals.css";
+import { inter, firaCode } from "./fonts";
 import { GoogleAnalyticsTracker } from "@/app/analytics";
 import Footer from "@/components/footer";
 import Script from "next/script";
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: "no",
+  themeColor: "black",
+};
 
 export const metadata = {
   title: {
@@ -9,21 +18,27 @@ export const metadata = {
     template: "%s | Churchment",
   },
   description: "O Churchment é uma plataforma moderna e completa para gestão de igrejas.",
-  keywords: ["Churchment", "gestão de igrejas", "plataforma para igrejas", "eventos da igreja", "ministérios"],
+  keywords: [
+    "Churchment",
+    "gestão de igrejas",
+    "plataforma para igrejas",
+    "eventos da igreja",
+    "ministérios",
+  ],
   authors: [{ name: "Churchment", url: "https://churchment.vercel.app/" }],
   creator: "Churchment",
   metadataBase: new URL("https://churchment.vercel.app/"),
   openGraph: {
     title: "Churchment - Gestão de Igrejas Moderna e Simples",
     description: "O Churchment é uma plataforma moderna e completa para gestão de igrejas.",
-    url: "https://churchment.com",
+    url: "https://churchment.vercel.app/",
     siteName: "Churchment",
     images: [
       {
-        url: "/opengraph-image.png",
+        url: "https://churchment.vercel.app/opengraph-image.png",
         width: 1200,
         height: 630,
-        alt: "Imagem da Churchment",
+        alt: "Imagem representando o Churchment",
       },
     ],
     locale: "pt_BR",
@@ -33,7 +48,7 @@ export const metadata = {
     card: "summary_large_image",
     title: "Churchment - Gestão de Igrejas",
     description: "Organize sua igreja de forma moderna com o Churchment.",
-    images: ["/opengraph-image.png"],
+    images: ["https://churchment.vercel.app/opengraph-image.jpg"],
     creator: "@churchment",
   },
   icons: {
@@ -45,7 +60,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt">
+    <html lang="pt" className={`${inter.variable} ${firaCode.variable}`}>
       <head>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-2VRCN8LVBN"
